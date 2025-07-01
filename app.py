f5fb360049fbfa228dd34d77d7b098a4576a01ab
Karan-Baid <151212469+Karan-Baid@users.noreply.github.com>
2025-07-01 09:20:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ if st.button("Summarize"):
         try:
             with st.spinner("Waiting...."):
                 if "youtube.com" in url:
-                    data=get_youtube_transcript(url)
+                    transcript = get_youtube_transcript(url)
+                    data = [Document(page_content=transcript)]
                 else:
                     loader=UnstructuredURLLoader(urls=[url],ssl_verify=False,
                                                  headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
